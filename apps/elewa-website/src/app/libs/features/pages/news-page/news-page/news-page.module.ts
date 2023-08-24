b71b9254f9b0e80c3bb945ef23a4e1ec2f2f18e5
Isaac <isaacongera717@gmail.com>
2023-08-24 10:09:29 +0300

    news page module component and routing created

---
+++ apps/elewa-website/src/app/libs/features/pages/news-page/news-page/news-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewsPageComponent } from './news-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    NewsPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NewsPageModule { }
