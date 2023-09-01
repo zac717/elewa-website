b968c90afdb2f16ce14e6794588a98cfa836c5e7
Isaac Ongera <117748841+zac717@users.noreply.github.com>
2023-09-01 13:20:18 +0300

    component and mock data

---
+++ libs/elements/layout/footer/src/lib/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'elewa-website-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent {}
